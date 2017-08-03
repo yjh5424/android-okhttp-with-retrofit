2ac125f96bb98e1ca484af76511832a83100b2a8
yjh5424 <yjh54240@naver.com>
2017-08-03 10:46:34 +0900

    add content

--- android_okhttp_retrofit/app/src/main/java/yjh/test/android_okhttp_retrofit/Service.java
+++ android_okhttp_retrofit/app/src/main/java/yjh/test/android_okhttp_retrofit/Service.java
@@ -1,9 +1,31 @@
 package yjh.test.android_okhttp_retrofit;
 
+import com.google.gson.JsonArray;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by dsm2016 on 2017-08-03.
  */
 
 public interface Service {
+
+    int HTTP_OK = 200;
+    int HTTP_CREATED = 201;
+    int HTTP_NO_CONTENT = 204;
+    int HTTP_BAD_REQUEST = 400;
+    int HTTP_INTERNAL_SERVER_ERROR = 500;
+
+    String SERVER_URL="http://13.124.15.202:8080/";
+
+
+    //you must use com.google.gson to use json
+    @GET("print")
+    Call<JsonArray> loadFood();
+
+    @GET("print")
+    Call<JsonArray> loadFoodCategory(@Query("") int num);
 
 }
